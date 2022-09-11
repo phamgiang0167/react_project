9a9c0873ef245b79ef44f3ed0de954bca0f6dcbd
phamgiang0167 <jagsiph@gmail.com>
2022-09-11 16:35:21 +0700

    commit

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,65 +1,68 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import '../style/student.css'
+import "../style/student.css";
+import { useState } from "react";
+import { useEffect } from "react";
+
 function Student(props) {
+  const [id, setId] = useState(0)
+  const [name, setName] = useState('')
+  const [classStudent, setClassStudent] = useState('')
+  const [listStudent, setListStudent] = useState([]);
+
+  useEffect(() => {
+    if(id > 0) {
+      const data = {
+        id: id,
+        name: name,
+        class: classStudent
+      }
+      setListStudent([
+        ...listStudent,
+        data
+      ])
+    }
+  }, [id])
+
+  const handleChangeName = (e) => {
+    const value = e.target.value
+    setName(value)
+  }
+
+  const handleChangeClass = (e) => {
+    const value = e.target.value
+    setClassStudent(value)
+  }
+
+  const handleOk = () => {
+    setId(id + 1)
+  }
   return (
-    <table id="customers" style={{width: '100%'}}>
-      <tr>
-        <th>id</th>
-        <th>name</th>
-        <th>class</th>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-      </tr>
-      <tr>
-        <td>Berglunds snabbköp</td>
-        <td>Christina Berglund</td>
-        <td>Sweden</td>
-      </tr>
-      <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Island Trading</td>
-        <td>Helen Bennett</td>
-        <td>UK</td>
-      </tr>
-      <tr>
-        <td>Königlich Essen</td>
-        <td>Philip Cramer</td>
-        <td>Germany</td>
-      </tr>
-      <tr>
-        <td>Laughing Bacchus Winecellars</td>
-        <td>Yoshi Tannamuri</td>
-        <td>Canada</td>
-      </tr>
-      <tr>
-        <td>Magazzini Alimentari Riuniti</td>
-        <td>Giovanni Rovelli</td>
-        <td>Italy</td>
-      </tr>
-      <tr>
-        <td>North/South</td>
-        <td>Simon Crowther</td>
-        <td>UK</td>
-      </tr>
-      <tr>
-        <td>Paris spécialités</td>
-        <td>Marie Bertrand</td>
-        <td>France</td>
-      </tr>
-    </table>
+    <Fragment>
+      <div>name</div>
+      <input style={{margin: '20px 0'}} onChange={handleChangeName}></input>
+      <div>class</div>
+      <input style={{margin: '20px 0'}} onChange={handleChangeClass}></input>
+      <div>
+        <button onClick={handleOk}>ok</button>
+      </div>
+      <table id="customers" style={{ width: "100%" }} >
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>class</th>
+        </tr>
+        {listStudent.map((ele, index) => {
+          return (
+            <tr>
+              <td>{ele.id}</td>
+              <td>{ele.name}</td>
+              <td>{ele.class}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </Fragment>
   );
 }
 
